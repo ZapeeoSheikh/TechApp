10c263d79e83b8d7c60bb8783d5f87210aa002d3
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-17 01:27:49 +0500

    update

--- lib/login.dart
+++ lib/login.dart
@@ -3,52 +3,68 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  static final Color mycolour = Color(0xFF151515);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "BMI Calculator",
+            ),
+            centerTitle: true,
+            backgroundColor: Color(0xFF303030),
+            elevation: 0,
+          ),
           body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: MyContainer(
-                    colour: Color(0xFF151515),
-                  ),
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: MyContainer(
+                        colour: mycolour,
+                      ),
+                    ),
+                    Expanded(
+                      child: MyContainer(
+                        colour: mycolour,
+                      ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: MyContainer(
-                    colour: Color(0xFF151515),
-                  ),
+              ),
+              Expanded(
+                child: MyContainer(
+                  colour: mycolour,
                 ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: MyContainer(
-              colour: Color(0xFF151515),
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: MyContainer(
-                    colour: Color(0xFF151515),
-                  ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: MyContainer(
+                        colour: mycolour,
+                      ),
+                    ),
+                    Expanded(
+                      child: MyContainer(
+                        colour: mycolour,
+                      ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: MyContainer(
-                    colour: Color(0xFF151515),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      )),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                width: double.infinity,
+                height: 70.0,
+                color: Color(0xFFEB1555),
+              )
+            ],
+          )),
     );
   }
 }
